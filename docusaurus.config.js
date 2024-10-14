1b4a1a302126105e4b988af3a62a5ed05ea51e69
editspace <editspace@protonmail.com>
2024-10-13 19:30:46 -0700

    forgot to commit changes to config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'The Industrialists', // Usually your GitHub org/user name.
+  organizationName: 'The-Industrialists', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
